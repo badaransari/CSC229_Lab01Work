b49138eb8d1a802a91cec32a6430b76d6401c6b1
soblab <soblab@Whit209-03.it.farmingdale.edu>
2022-02-02 13:21:52 -0500

    Print sum of Prime Numbers

--- src/main/java/com/mycompany/csc229_lab1work/LabWork.java
+++ src/main/java/com/mycompany/csc229_lab1work/LabWork.java
@@ -3,8 +3,24 @@ package com.mycompany.csc229_lab1work;
 
 
 public class LabWork {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
-    }
     
+    static int getSumOfPrimes(int n){
+            int sum = 0;
+            for (int i = 2; i <= n; i++)
+                if (isPrime(i))
+                  sum = sum + i;
+                  
+            return sum;
+    }
+    static boolean isPrime(int x ) {
+           for (int i=2; i<x; i++)
+               if (x%i == 0)
+                  return false;
+           return true;
+}
+  
+
+  public static void main (String[] args) {
+    System.out.println("The prime sum to 10 is" + getSumOfPrimes(10));
+        }
 }
